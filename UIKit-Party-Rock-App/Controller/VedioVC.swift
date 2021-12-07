becd08e1887f7d016cb3cfc0e24039ce2035744f
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2021-12-06 20:38:30 -0800

    Party Rock UI Construction

---
+++ UIKit-Party-Rock-App/Controller/VedioVC.swift
@@ -0,0 +1,29 @@
+//
+//  VedioVC.swift
+//  UIKit-Party-Rock-App
+//
+//  Created by Pavan Mikkilineni on 06/12/21.
+//
+
+import UIKit
+
+class VedioVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
